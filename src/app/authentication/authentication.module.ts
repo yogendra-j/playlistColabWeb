cec42e9bef45588abf1a2ec02487790c598eb0db
Yogendra Jaiswal <yogendra.jaiswal.503@gmail.com>
2022-06-20 13:14:45 +0530

    login component added

--- src/app/authentication/authentication.module.ts
+++ src/app/authentication/authentication.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AuthenticationRoutingModule } from './authentication-routing.module';
+import { LoginComponent } from './login/login.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LoginComponent
+  ],
   imports: [
     CommonModule,
     AuthenticationRoutingModule
